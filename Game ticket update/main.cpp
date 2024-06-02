a7eed2859524a15cbb9e8fb39d353f9043693269
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-06-02 12:45:49 +0300

    Add files via upload

---
+++ Game ticket update/main.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <locale.h>
+
+
+int main() {
+    // Локализация 
+    std::locale::global(std::locale("ru_RU.UTF-8"));
+    
+    
+    std::wstring name;
+    
+    std::wcout << L"Как тебя зовут? ";
+    std::getline(std::wcin, name);
+    
+    // c помощью многопоточности выводим: 
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(900));
+   
+    // Выводим вводимые данные в name;
+    
+    
+    std::random_device rd;  // Источник случайной энтропии (может не быть доступным на всех платформах)
+    std::mt19937 gen(rd()); // Используем Mersenne Twister 19937 в качестве генератора
+    std::uniform_int_distribution<int> dis(1, 100); // Равномерное распределение от 1 до 100 (включительно)
+
+    // Генерация случайных чисел
+    int random_number = dis(gen);
+    if(random_number == 55) {
+        std::wcout << L"Привет " << name << L" твой код " <<  random_number << L" ты выиграл!" << std::endl;
+    } else {
+        std::wcout << L"Привет " << name << L" твой код " <<  random_number << L" и ты проиграл!" << std::endl;
+    }
+    
+   
+    
+    return 0;
+}
+
