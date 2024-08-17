8bbc1fab40bc3cbcb1f1b1241d0787cc4d80b658
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-08-17 13:05:11 +0300

    Add files via upload

---
+++ Try catch/Try catch/Try catch.cpp
@@ -0,0 +1,26 @@
+﻿// Try catch.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() {
+
+    setlocale(LC_ALL, "RU");
+    try {
+        int age = 8;
+
+        if (age >= 18) {
+            std::cout << "Успешно!" << std::endl;
+        }
+
+        else {
+            throw(age);
+        }
+    }
+    catch (int MyNumber) {
+        std::cout << "Ошибка!!" << std::endl;
+    }
+
+
+}
+
