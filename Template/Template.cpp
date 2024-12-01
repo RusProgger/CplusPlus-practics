604b82240682ddfe845193d8aa156cc60d0d4a7f
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-01 20:57:28 +0200

    Add files via upload

---
+++ Template/Template.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include "colors.h";
+
+template <typename T>
+ T add(T a, T b) {
+     return a + b;
+}
+
+int main() {
+
+    std::locale::global(std::locale(""));
+
+    int result_int = add(20, 15);
+
+    double result_double = add(1.7, 2.5);
+    std::cout << "Подождите, идет подсчет результатов..." << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << RED << "Это переменная 1 = " << result_double << RESET << std::endl;
+    std::cout << GREEN << "Это переменная 2 = " << result_int << RESET << std::endl;
+
+}
---
+++ Template/colors.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef COLORS_H
+
+#define COLORS_H
+
+#define RESET "\033[0m"
+
+#define RED "\033[31m"
+
+#define GREEN "\033[32m"
+
+#define YELLOW "\033[33m"
+
+#define RESET "\033[0m"
+
+#endif
