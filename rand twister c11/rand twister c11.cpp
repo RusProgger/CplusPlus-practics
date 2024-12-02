a83792c9b33ea2e975cf04d4875f9fa8ce0bb5a0
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-02 22:41:57 +0200

    Add files via upload

---
+++ rand twister c11/rand twister c11.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <random>
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+
+int main() {
+
+	std::locale::global(std::locale(""));
+
+	int number = 0; // Для пользователя 
+
+	// Создаем генератор случайных чисел
+	std::mt19937 generator(time(0));
+	std::uniform_int_distribution<> dist{1, 10};
+
+	int random_number = dist(generator);
+
+	/*std::cout << random_number << std::endl;*/
+
+
+	do {
+		std::cout << "Введите число: ";
+		std::cin >> number;
+
+		if (number > random_number) {
+			std::cout << "Число слишком большое...\n";
+		}
+		else if(number < random_number) {
+			std::cout << "Число слишком маленькое...\n";
+		}
+
+	} while (random_number != number);
+
+#ifdef _WIN32
+	system("pause");
+#else
+	std::cin.get();
+#endif // _WIN32
+
+
+	return EXIT_SUCCESS;
+}
