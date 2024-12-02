2edab3ad8de56c09cc5a346ce01550832c0b2019
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-02 22:03:16 +0200

    Add files via upload

---
+++ Random Srand/Random Srand.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::locale;
+
+int main() {
+
+	locale::global(locale(""));
+
+	srand(time(0));
+
+
+	cout << rand() % 10 << endl; // тогда данный результат вывода от 0 до 9
+
+
+
+
+// создадим макрос для кроссплатформенности
+
+#ifdef _WIN32
+
+	system("pause");
+	
+
+#else 
+
+	cin.get();
+
+#endif // _WIN32
+
+
+	return EXIT_SUCCESS;
+}
