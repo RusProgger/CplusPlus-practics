a1f2ee77531050ad0a02d832efb3d1ce05a9b358
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-21 20:15:41 +0200

    Add files via upload

---
+++ classBooks/classBooks.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+class Books {
+	public:
+		int year;
+		std::string type;
+
+		void get_info() {
+			std::cout << "Название: " << type << ". Год: " << year << "\n";
+		}
+};
+
+
+int main()
+{
+   
+	std::locale::global(std::locale(""));
+
+	Books programming;
+	programming.type = "Системное программирование";
+	programming.year = 2019;
+
+	programming.get_info();
+
+}
+
