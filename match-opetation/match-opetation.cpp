7f0197e18ec9e3cade85f110463c711d059ba856
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-25 00:00:24 +0200

    Add files via upload

---
+++ match-opetation/match-opetation.cpp
@@ -0,0 +1,14 @@
+﻿// match-opetation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() {
+
+    std::string h = "Hello";
+    std::string t = h;
+
+    t += ", world!";
+    std::cout << t << std::endl;
+}
+
