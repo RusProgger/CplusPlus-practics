b28b72d295990ee0b1996c5df6622202eba50f5d
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-24 19:50:10 +0200

    Add files via upload

---
+++ Symbol - programms enter/Symbol - programms enter.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+int main() {
+
+	std::locale::global(std::locale(""));
+
+	// Просим ввод пользователя
+
+	do {
+		char symbol;
+		std::cout << "Введите символ с клавиатуры: ";
+
+		std::cin >> symbol;
+
+		std::cout << "Ваш код символа: " << static_cast<int>(symbol) << std::endl;
+	} while (true);
+
+	
+
+	return EXIT_SUCCESS;
+}
