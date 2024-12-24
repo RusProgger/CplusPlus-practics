efe4198453cc6191c844641619bb5e3f1d802344
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-25 00:09:20 +0200

    Add files via upload

--- match-opetation/match-opetation.cpp
+++ match-opetation/match-opetation.cpp
@@ -1,7 +1,5 @@
-﻿// match-opetation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 
 int main() {
 
@@ -11,6 +9,20 @@ int main() {
     t += ", world!";
     std::cout << h << std::endl;
     std::cout << t << std::endl;
+    std::cout << std::endl;
+    std::cout << "***********************************" << std::endl;
+
+    // Vector
+
+    std::vector<std::string> m = { "a", "b", "c", "d", "i"};
+
+    std::vector<std::string> v;
+
+    v = m;
+
+    v[0] = "d";
+    std::cout << m[0] << m[1] << std::endl;
+    std::cout << v[0] << v[1] << std::endl;
 
 }
 
