f1bd8663dc245db0a8377c32c543a4b5a207e799
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-01-03 13:02:59 +0200

    Update Symbol - programms enter.cpp

--- Symbol - programms enter/Symbol - programms enter.cpp
+++ Symbol - programms enter/Symbol - programms enter.cpp
@@ -6,11 +6,12 @@ int main() {
 	std::locale::global(std::locale(""));
 
 	// Просим ввод пользователя
-	
+
+	// loop enter symbol: 
 	do {
 		char symbol;
 		std::cout << "Введите символ с клавиатуры: ";
-
+		// Просим пользователя вести символ
 		std::cin >> symbol;
 
 		std::cout << "Ваш код символа: " << static_cast<int>(symbol) << std::endl;
