063a0212c7ff9985b91708e644840b5228a35df6
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-03-05 00:26:57 +0200

    Add files via upload

---
+++ while-cucle/while-cucle/while-cucle.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>  
+
+int main() {
+
+    std::locale::global(std::locale(""));
+   
+    int width;
+
+    std::cout << "Установите ширину таллицы: ";
+
+    std::cin >> width;
+
+    
+    for (int i = 1; i <= 10; i++) {
+        
+        for (int j = 1; j <= 10; j++) {
+           
+            std::cout << std::setw(width) << i * j;
+
+        }
+        std::cout << std::endl;  
+    }
+
+    return 0;
+}
