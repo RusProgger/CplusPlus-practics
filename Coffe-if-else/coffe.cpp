90f3256c3cbd2d28fa0a2d95d1610f091c66e28d
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-06-30 13:16:23 +0300

    Add files via upload

---
+++ Coffe-if-else/coffe.cpp
@@ -0,0 +1,41 @@
+include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // ввод напитка: "чай" или "кофе"
+	string drink;
+	cin >> drink;
+
+	if (drink == "чай") 
+	{
+        // ввод типа чая: "черный" или "зеленый"
+        string tea_type;
+        cin >> tea_type;
+
+        if (tea_type == "черный") 
+		{
+			cout << "Ваш черный чай готов";
+		}
+		if (tea_type == "зеленый") 
+		{
+			cout << "Ваш зеленый чай готов";
+		}
+	}
+
+	if (drink == "кофе") 
+	{
+        // ввод типа кофе: "эспрессо" или "латте"
+        string coffee_type;
+        cin >> coffee_type; 
+
+        if (coffee_type == "эспрессо") 
+		{
+			std::cout << "Ваш кофе эспрессо готов";
+		}
+		if (coffee_type == "латте") 
+		{
+			std::cout << "Ваш кофе латте готов";
+		}        
+	}
+}
