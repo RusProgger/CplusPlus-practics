20807d1c13804c4c827c9a38221ab9e130e55f7f
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-07-01 13:50:36 +0300

    Add files via upload

---
+++ Гипотеза Коллатца/gipotesa.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << n;
+    while(n != 1) {
+        if(n % 2 ==0) {
+            n /= 2;
+        } else {
+        
+           n = 3 * n + 1; 
+        }
+       
+        if(n != 1) {
+            std::cout << " -> " << n;
+        } else {
+        
+            std::cout << " -> " << n << ".";
+        }
+    }
+    
+
+}
