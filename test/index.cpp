abd0c12f101b539a7e53ae30e8e8fabf8db86473
RusProgger <ruslantimka92@gmail.com>
2025-07-31 00:56:59 +0300

    Отключение локации

--- test/index.cpp
+++ test/index.cpp
@@ -1,15 +1,17 @@
 #include <iostream> 
 #include <string>
-#include <locale.h>
+// #include <locale>
 
 int main() {
-   std::locale::global(std::locale("Russian"));
+//    std::locale::global(std::locale("Russian"));
    std::string name;
    int password;
    std::cin >> name;
    std::cin >> password;
 
    if(name != "Alex" && password != 8871) {
-
+        for(int count = 3; count > 3; count--) {
+            std::cout << count << " Попытка - неверные данные. повторите.\n";
+        }
    }
 }
