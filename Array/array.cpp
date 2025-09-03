300e31c97d3ac2481e8f239ba7c1e9b4820e853d
RusProgger <ruslantimka92@gmail.com>
2025-09-03 23:14:43 +0300

    Добавлен массив

--- Array/array.cpp
+++ Array/array.cpp
@@ -3,8 +3,14 @@
 
 
 int main() {
-    
 
+    const int n = 8;
+
+    float a[n] = { 100, 200, 440, 51, 96, 29, 47, 153 };
+    
+    for(int i = 0; i < n; i++) {
+        std::cout << a[i] << std::endl;
+    }
 
     return 0;
 }
