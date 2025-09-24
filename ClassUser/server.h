d79932fef8a06721b629003b1e5027bf22370b81
RusProgger <ruslantimka92@gmail.com>
2025-09-24 18:33:31 +0300

    Исправлена ошибка

--- ClassUser/server.h
+++ ClassUser/server.h
@@ -21,7 +21,7 @@ class Server {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         std::cout << "Server: " << std::setw(5) << serverInfo << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-        std::cout << "ipAddress : " << std::setw(5) << ipAddress << std::endl;
+        std::cout << "ipAddress: " << std::setw(5) << ipAddress << std::endl;
     }
 
 };
