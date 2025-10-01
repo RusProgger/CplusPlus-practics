6e0e3297bc214d6fea297da03715e215d5821327
RusProgger <ruslantimka92@gmail.com>
2025-10-01 09:40:55 +0300

    Добавляем елементы

--- Vector_cin/vectorCin.cpp
+++ Vector_cin/vectorCin.cpp
@@ -5,7 +5,17 @@ int main() {
 
     int numberVector;
     std::vector <int> v {numberVector};
-    
+
+    std::cout << "Enter vector size: ";
+    std::cin >> numberVector;
+
+    for(int i = 0; i < numberVector; i++) {
+        std::cout << "Enter vector number: ";
+        std::cin >> v[i];
+    }
+
+    std::cout << ""; 
+
 
 
     return 0;
