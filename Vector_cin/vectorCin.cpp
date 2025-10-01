41f1603df210afd55d9564599db4de564bd336cc
RusProgger <ruslantimka92@gmail.com>
2025-10-01 10:00:34 +0300

    добавление вектора

--- Vector_cin/vectorCin.cpp
+++ Vector_cin/vectorCin.cpp
@@ -4,17 +4,22 @@
 int main() {
 
     int numberVector;
-    std::vector <int> v {numberVector};
-
     std::cout << "Enter vector size: ";
     std::cin >> numberVector;
+
+    std::vector <int> v(numberVector);
 
     for(int i = 0; i < numberVector; i++) {
         std::cout << "Enter vector number: ";
         std::cin >> v[i];
     }
 
-    std::cout << ""; 
+
+    for(int j = 0; j < numberVector; j++) {
+        std::cout << "Number element vector: " << j << v[j] << std::endl; 
+    }
+
+    std::cout << "Count vector: " << numberVector << std::endl;
 
 
 
