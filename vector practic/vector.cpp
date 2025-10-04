3c4d4421b1d3dd74bae645a5f89e0f0d23c8b634
RusProgger <ruslantimka92@gmail.com>
2025-10-04 12:40:46 +0300

    Добавлен Vector

---
+++ vector practic/vector.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+    int number;
+    std::vector <int> numberEnter;
+
+    std::cout << "Enter number: ";
+    std::cin >> number;
+
+    for(int i = 0; i < number; i++) {
+        int var; 
+        
+        std::cin >> var;
+        numberEnter.push_back(var);
+    }
+
+    // не отсортированный вектор
+
+    for(int j = 0; j < number; j++) {
+        std::cout << "Vector " << j << " = " << numberEnter[j] << "\n";
+    }
+
+    // отсортированный вектор
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::sort(numberEnter.begin(), numberEnter.end());
+
+     for(int j = 0; j < number; j++) {
+        std::cout << "Vector " << j << " = " << numberEnter[j] << "\n";
+    }
+
+    std::cin.get();
+    return 0;
+}
+
+
