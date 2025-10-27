ed25aaffd40f26e7cd3da10799c16ba7627e5119
RusProgger <ruslantimka92@gmail.com>
2025-10-28 01:14:48 +0200

    Добавлена структура

--- StructName/main.cpp
+++ StructName/main.cpp
@@ -32,9 +32,39 @@ bool isAdult(const Person& p) {
 
 int main() {
 
+
     // обьявляем структуру
 
-    
+    Person ivan = {"Ivan", 1990,  72.3, 175, 20, "green", "Volga"};
+    Person roma = {"Roman", 1988,  71.9, 178, 31, "green", "Tikva"};
+    Person vania = {"Ivan", 1970,  81.3, 181.1, 50, "grey", "Kipr"};
+    Person artem = {"Ivan", 2005,  72.3, 175, 15, "green", "Moskov"};
 
+    // вывод структуры
+
+    // std::cout << "-----------------------------" << std::endl;
+    // if(!isAdult(ivan)) {
+    //     std::cout << " is Adult" << std::endl;
+    // } else {
+    //     std::cout << " Adult" << std::endl;
+    // }
+
+    
+    std::cout << "Name: " << ivan.name << "\nDate of birth: " << ivan.date_of_birth << "\nWeight: " << ivan.weight << "\nHeight: " << ivan.height << "\nAge: " << ivan.age << "\nColor eye: " << ivan.color_eye << "\nCity: " << ivan.city_user << "\n";
+
+    std::cout << "-----------------------------" << std::endl;
+
+    std::cout << "Name: " << roma.name << "\nDate of birth: " << roma.date_of_birth << "\nWeight: " << roma.weight << "\nHeight: " << roma.height << "\nAge: " << roma.age << "\nColor eye: " << roma.color_eye << "\nCity: " << roma.city_user << "\n";
+
+    std::cout << "-----------------------------" << std::endl;
+
+    std::cout << "Name: " << vania.name << "\nDate of birth: " << vania.date_of_birth << "\nWeight: " << vania.weight << "\nHeight: " << vania.height << "\nAge: " << vania.age << "\nColor eye: " << vania.color_eye << "\nCity: " << vania.city_user << "\n";
+
+    std::cout << "-----------------------------" << std::endl;
+
+    std::cout << "Name: " << artem.name << "\nDate of birth: " << artem.date_of_birth << "\nWeight: " << artem.weight << "\nHeight: " << artem.height << "\nAge: " << artem.age << "\nColor eye: " << artem.color_eye << "\nCity: " << artem.city_user << "\n";
+
+
+    std::cin.get(); // для задержки экрана
     return 0;
 }
