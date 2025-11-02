8cd1e476f45aed65ea3bd5087fbc3b5231b5e380
RusProgger <ruslantimka92@gmail.com>
2025-11-02 13:00:52 +0200

    Работа с voidFucntion в С++

--- voidFunction/main.cpp
+++ voidFunction/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+void numberInt(int n) {
+    std::cout << "Number: " << n << std::endl;
+}
+
+void numberInt(float n) {
+    std::cout << "Number: " << n << std::endl;
+}
+
+void numberInt(double n) {
+    std::cout << "Number: " << n << std::endl;
+}
+
+void numberInt(std::string n) {
+    std::cout << "Number: " << n << std::endl;
+}
 
 
 int main() {
 
+    numberInt(100);
 
 
     return 0;
