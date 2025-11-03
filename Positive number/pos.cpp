ed9e32e2cbf4d7fab071c11194f436f4609d26d4
RusProgger <ruslantimka92@gmail.com>
2025-11-03 12:58:15 +0200

    Добавлен новый проект позитывное ли число?

--- Positive number/pos.cpp
+++ Positive number/pos.cpp
@@ -2,6 +2,21 @@
 
 int main() {
     
+    int x;
+
+    std::cout << "Enter number: ";
+    std::cin >> x;
+
+    if(x >= 1) {
+        int summ = 0;
+        while(x > 0) {
+            summ += x;
+            --x;
+        }
+        std::cout << summ;
+    } else {
+        std::cout << "Please enter positive  number";
+    }
 
 
     std::cin.get();
