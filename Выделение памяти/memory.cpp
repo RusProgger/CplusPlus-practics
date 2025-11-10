a5956345ba0b8d11d900b9b08176705def7b7894
RusProgger <ruslantimka92@gmail.com>
2025-11-10 11:15:09 +0200

    Работа с массивом и памятью

--- Выделение памяти/memory.cpp
+++ Выделение памяти/memory.cpp
@@ -8,13 +8,26 @@ int main() {
     const int num = 5;
 
     int arr[num];
+    int a = 0;
 
-    for(int i = 0; i < num; i++) {
-        std::cin >> arr[i];
+    // for(int i = 0; i < num; i++) {
+    //     std::cin >> arr[i];
+    // }
+
+    // for(int i = 0; i < num; i++) {
+    //     std::cout << arr[i] << " ";
+    // }
+
+    while(a < num) {
+        std::cin >> arr[a];
+        a++;
     }
 
-    for(int i = 0; i < num; i++) {
-        std::cout << arr[i] << " ";
+    a = 0;
+
+    while(a < num) {
+        std::cout << arr[a] << " ";
+        a++;
     }
-   
+
 }
