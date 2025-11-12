6eea839a21998fbb968e5c3bbdab2f44f71d4b58
RusProgger <ruslantimka92@gmail.com>
2025-11-12 23:44:34 +0200

    Работа с вектором

--- Vector20/vector.cpp
+++ Vector20/vector.cpp
@@ -16,8 +16,12 @@ int main() {
 
     // Тут дописать вектор user с именами пользователя.
 
+    std::vector<std::string> vect {"Ivan", "Irina", "Denis", "Alena", "Julia"};
 
+    // вывод вектора 
 
+    std::cout << "Vector user male: " << vect[0] << vect[2] << " ";
+    
     std::cin.get();
     return 0;
 }
