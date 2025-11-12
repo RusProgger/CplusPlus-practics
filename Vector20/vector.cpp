c7fe14fef4231a6da5bb99fe31aee44c2961cf5f
RusProgger <ruslantimka92@gmail.com>
2025-11-12 23:32:55 +0200

    Работа с вектором

--- Vector20/vector.cpp
+++ Vector20/vector.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
+
+void out_vector(const std::vector<T>& vector_user) {
+    for(size_t i{}; i < vector_user; i++) {
+        std::cout << vector_user[i] << " ";
+    }
+
+    std::cout << std::endl;
+}
+
 
 int main() {
-    
+
+    // Тут дописать вектор user с именами пользователя.
+
+
 
     std::cin.get();
     return 0;
