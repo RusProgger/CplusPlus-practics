ff1cb42658fc99ced0b9d5b716cefeee1cde0bfb
RusProgger <ruslantimka92@gmail.com>
2025-11-25 01:36:02 +0200

    Работа с памятью в С++

--- Memory/memoryCpp.cpp
+++ Memory/memoryCpp.cpp
@@ -2,15 +2,20 @@
 
 int main() {
 
-   int* num = new int;
-   *num = 100;
-   std::cout << *num << std::endl;
+//    int* num = new int;
+//    *num = 100;
+//    std::cout << *num << std::endl;
 
     
-    delete num;
+//     delete num;
     
     // очищаем висячий указатель
 
-    num = nullptr;
+    // num = nullptr;
+
+    std::string* strings = new std::string[5];
+
+    
+
     return 0;
 }
