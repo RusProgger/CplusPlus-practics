c28e3e8dea215a483f9e03d46840eea4c261397c
RusProgger <ruslantimka92@gmail.com>
2026-02-15 15:47:56 +0200

    Номер символа

---
+++ Symbol_String/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+// Номер символа
+
+int main() {
+
+    std::string text;
+    std::cout << "Enter text: ";
+    std::getline(std::cin, text);
+
+    for(char symbol : text) {
+        std::cout << symbol << "\t" << static_cast<int>(symbol) << "\n";
+    }
+
+
+    return 0;
+}
