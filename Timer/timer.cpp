d69f551373708c103975b9d1119fdba5c3b12ac6
RusProgger <ruslantimka92@gmail.com>
2026-03-06 14:40:19 +0200

    Таймер на С++

--- Timer/timer.cpp
+++ Timer/timer.cpp
@@ -4,9 +4,21 @@
 
 int main() {
 
-    unsigned time {0};
-        
+     int time {0};
+    std::cout << "Input number: ";
+    std::cin >> time;
+    
+    if(time < 0) {
+        std::cout << "Error. Minimum enter number 1" << std::endl;
+    } else {
 
+        for(int i = time; i >= 1; i--) {
+            std::cout << "Timer: " << i << "   \r" << std::flush;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        std::cout << "Timer over!";
+    }
 
     return 0;
 }
