a575ec6f65aca094eb51a86d81b0ed62a798df46
RusProgger <ruslantimka92@gmail.com>
2026-03-10 11:34:37 +0200

    Таймер на С++

--- UserTimer/userTimer.cpp
+++ UserTimer/userTimer.cpp
@@ -7,6 +7,15 @@ int main() {
     std::cout << "Input number timer: ";
     std::cin >> userInputTime;
 
+    if(userInputTime < 0) {
+        std::cout << "Error. Minimum number 1. ";    
+        return 0;
+    }
+
+    for(int timer = userInputTime; timer >= 0; timer--) {
+        std::cout << "Timer start: " << timer << "\r" << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     return 0;
 }
