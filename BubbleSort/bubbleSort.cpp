06e1d7f8269d19d0d06d0d4dee318bb213d52772
RusProgger <ruslantimka92@gmail.com>
2026-03-25 12:13:21 +0200

    Пузырьковая сортировка массива - "Просим пользователя выделить память"

--- BubbleSort/bubbleSort.cpp
+++ BubbleSort/bubbleSort.cpp
@@ -4,5 +4,15 @@ int main() {
 
     // Пузырьковая сортировка
 
+    int allocateNumber {};
+    // Спрашиваем у пользователя сколько байтов памяти он хочет выделить для массива
+    std::cout << "How much memory do you want to allocate in bytes? ";
+    std::cin >> allocateNumber;
+
+    int* allocateNumber_p = new int[allocateNumber];
+    
+
+    delete[] allocateNumber_p;
+
     return 0;
 }
