75e86b77b21d3475630d6b01caa9f53094978c71
RusProgger <ruslantimka92@gmail.com>
2026-04-10 18:47:14 +0300

    Алгоритм для поиска символа

--- Алгоритм для подсчитывания символов/Алгоритм для подсчитывания символов/Алгоритм для подсчитывания символов.cpp
+++ Алгоритм для подсчитывания символов/Алгоритм для подсчитывания символов/Алгоритм для подсчитывания символов.cpp
@@ -11,14 +11,30 @@ int main() {
 
     Locale();
 
-    std::string _str;
+    std::string _str{};
+
+    uint16_t maxCount{};
 
     std::cout << "Введите строку: ";
     std::getline(std::cin, _str);
 
-    
+    // поиск символа
 
+    char symbol{};
+    std::cout << "Введите символ который вы хотите найти: ";
+    std::cin >> symbol;
 
+    // Реализация алгоритма 
+
+    for (int i = 0; i < _str.size(); i++) {
+        if (_str[i] == symbol) {
+            maxCount += 1;
+        }
+    }
+
+    std::cout << "По вашему запросу нашло: " << maxCount <<  " символа " << symbol;
+
+    std::cin.get();
     
     return 0;
 }
