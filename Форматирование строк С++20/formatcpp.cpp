b6b3861721986d8e171f7925b05b815a8152c370
RusProgger <ruslantimka92@gmail.com>
2026-04-14 21:24:49 +0300

    This is a formatted string in C++20.

--- Форматирование строк С++20/formatcpp.cpp
+++ Форматирование строк С++20/formatcpp.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <format>
+#include <string>
+
 
 int main() {
 
-    
+    std::string text = "Hello";
 
+    std::cout << std::format("{}, this is a formatted string in C++20.", text);
 
     return 0;
 }
