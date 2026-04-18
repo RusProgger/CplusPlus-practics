37469842a6fc0dc83b683a94ab65c136d491f412
RusProgger <ruslantimka92@gmail.com>
2026-04-18 15:41:59 +0300

    We allocate memory for the matrix”

--- Matrix/matrix.cpp
+++ Matrix/matrix.cpp
@@ -11,9 +11,17 @@ void locale() {
 
 int main() {
     locale();
+
+    int row {};
+    int collum {};
     std::cout << "Введите размер строки матрицы: ";
+    std::cin >> row;
+    std::cout << "Введите размер колонки матрицы: ";
+    std::cin >> collum;
 
+    // We allocate memory for the matrix”
 
+    int* matrix = new int[row * collum];
 
     return 0;
 }
