2e492318cf3400b86a64df037704a1eefffa8500
RusProgger <ruslantimka92@gmail.com>
2026-04-18 15:35:31 +0300

    Create matrix row

--- Matrix/matrix.cpp
+++ Matrix/matrix.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+
+void locale() {
+
+    SetConsoleCP(1251);        
+    SetConsoleOutputCP(1251); 
+
+}
 
 int main() {
+    locale();
+    std::cout << "Введите размер строки матрицы: ";
 
-    
     return 0;
 }
