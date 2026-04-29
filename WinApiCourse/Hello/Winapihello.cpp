7c82a8918124a05934fcc0888b98cf2f1c5a6d4e
RusProgger <ruslantimka92@gmail.com>
2026-04-29 19:01:23 +0300

    Welcome to WinApi

--- WinApiCourse/Hello/Winapihello.cpp
+++ WinApiCourse/Hello/Winapihello.cpp
@@ -1,9 +1,17 @@
 #include <windows.h>
 #include <format>
 #include <string>
+#include <iostream>
 
 int main() {
 
-    
+    std::string user {};
+    std::cout << "Enter userName: ";
+    std::cin >> user;
+
+    std::string msg = std::format("Hello {}", user);
+
+    MessageBoxA(NULL, msg.c_str(), "Window", MB_OK);
+
     return 0;
 }
