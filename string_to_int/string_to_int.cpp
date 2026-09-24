82b0a7d61814eac744611a09588e7910e565b35c
RusProgger <ruslantimka92@gmail.com>
2026-09-24 22:52:43 +0300

    String to int

--- string_to_int/string_to_int.cpp
+++ string_to_int/string_to_int.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <print>
+#include <string>
 
+
+/*
+
+std::stoi("123");       // string → int
+std::stol("123");       // string → long
+std::stoll("123");      // string → long long
+
+std::stof("3.14");      // string → float
+std::stod("3.14");      // string → double
+std::stold("3.14");     // string → long double
+
+
+*/
 
 int main() {
 
-    
+    std::string number {"100"};
+
+    // string to int 
+
+    int number_int = std::stoi(number);
+
+    std::print("Output: {}", number_int + 5);
 
 }
